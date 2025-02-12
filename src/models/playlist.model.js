9f8217b8ca659aae06e9f2dca38e6c4b4e8f60ec
Deepthy Nair <dpthynair@gmail.com>
2025-02-12 12:24:44 +0100

    Added models

---
+++ src/models/playlist.model.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const playlistSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    video: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+playlistSchema.plugin(mongooseAggregatePaginate);
+
+export const Playlist = mongoose.model("Playlist", playlistSchema);
